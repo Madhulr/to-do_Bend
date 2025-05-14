a6e0e2c07038952081e6514bdaf575d3c89f7e5c
Madhulr <madhubhagiratha@gmail.com>
2025-05-14 23:11:38 +0530

    Add Superuser creation to build script.

--- build.sh
+++ build.sh
@@ -8,5 +8,16 @@ pip install -r requirements.txt
 # Run migrations
 python manage.py migrate
 
+# Create superuser if it doesn't exist
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-admin}
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-admin@example.com}
+DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-admin}
+
+python manage.py shell << END
+from django.contrib.auth.models import User
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+END
+
 # Collect static files
 python manage.py collectstatic --no-input 
